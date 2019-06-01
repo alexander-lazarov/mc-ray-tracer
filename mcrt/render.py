7e2b8cdd79f61f627ef6bad9c3b120848efca3bd
Alexander Lazarov <alexander.lazaroff@gmail.com>
2019-06-01 23:48:20 +0300

    Make render a bit faster

--- mcrt/render.py
+++ mcrt/render.py
@@ -26,25 +26,28 @@ class Renderer:
 
         self.camera = (0, 0, -1)
         self.screen = (0, 0, 0)
-        self.screen_w = 1.00
-        self.screen_h = 0.75
+        self.projection_w = 1.00
+        self.projection_h = 0.75
+
 
     def render(self):
         img = Image.new("RGB", (self.image_w, self.image_h))
 
         data = []
+        ray = Ray(self.camera, (1, 1, 1))
+
+        step_w = self.projection_h / self.image_h
+        step_h = self.projection_w / self.image_w
 
         for y in range(self.image_h):
+            yoffset = (y - (self.image_h / 2)) * step_w
+
             for x in range(self.image_w):
-                xoffset = (x - (self.image_w / 2)) * (self.screen_w / self.image_w)
-                yoffset = (y - (self.image_h / 2)) * (self.screen_h / self.image_h)
+                xoffset = (x - (self.image_w / 2)) * step_h
 
-                pixel = (xoffset,
-                        yoffset,
-                        self.screen[2]
-                        )
+                pixel = (xoffset, yoffset, self.screen[2])
 
-                ray = Ray(self.camera, subtract3(pixel, self.camera))
+                ray.direction = subtract3(pixel, self.camera)
 
                 if self.scene.intersect(ray):
                     data.append((255, 255, 255))
